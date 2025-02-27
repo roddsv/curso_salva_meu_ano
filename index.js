255614fd659c4bf7a4cdf36289d5c283def79f27
Rodrigo Soares <adsrodsoares@gmail.com>
2025-02-26 22:10:12 -0300

    Ajuste de carrossel

--- index.js
+++ index.js
@@ -4,35 +4,35 @@ document.addEventListener("DOMContentLoaded", function () {
   const nextButton = document.querySelector(".carousel-btn.next");
   const testimonials = track.children;
   const items = testimonials.length;
+
   track.style.transform = `translateX(0px)`;
-  currentIndex = 0;
-  setTimeout(updateCarousel, 100);
-  currentIndex = 0;
-  updateCarousel();
-  track.scrollLeft = 0;
+        currentIndex = 0;
+        setTimeout(updateCarousel, 100);
+        currentIndex = 0;
+        updateCarousel();
+        track.scrollLeft = 0;
 
-  function updateCarousel() {
-      const videoWidth = 320; // Largura de um vídeo
-      const visibleVideos = 2; // Quantidade de vídeos visíveis
-      const containerWidth = videoWidth * visibleVideos;
-      track.style.width = `${items * videoWidth}px`;
-      track.style.transform = `translateX(${-currentIndex * videoWidth}px)`;
-      prevButton.style.display = currentIndex === 0 ? 'none' : 'block';
-      nextButton.style.display = currentIndex >= items - visibleVideos ? 'none' : 'block';
-      prevButton.style.display = 'block';
-      nextButton.style.display = currentIndex === items - 1 ? 'none' : 'block';
-  }
+        function updateCarousel() {
+            const videoWidth = 320;
+            const visibleVideos = 2;
+            const containerWidth = videoWidth * visibleVideos;
+            track.style.width = `${items * videoWidth}px`;
+            track.style.transform = `translateX(${-currentIndex * videoWidth}px)`;
+            prevButton.style.display = currentIndex === 0 ? 'none' : 'block';
+            nextButton.style.display = currentIndex + visibleVideos >= items ? 'none' : 'block';
+            prevButton.style.display = 'block';
+            nextButton.style.display = currentIndex === items - 1 ? 'none' : 'block';
+        }
 
-  function moveCarousel(direction) {
-      if ((direction === 1 && currentIndex < items - 1) || (direction === -1 && currentIndex > 0)) {
-          currentIndex += direction;
-          updateCarousel();
-      }
-  }
+        function moveCarousel(direction) {
+            if ((direction === 1 && currentIndex < items - 1) || (direction === -1 && currentIndex > 0)) {
+                currentIndex += direction;
+                updateCarousel();
+            }
+        }
 
-  prevButton.addEventListener('click', function() { moveCarousel(-1); });
-  nextButton.addEventListener('click', function() { moveCarousel(1); });
+        prevButton.addEventListener('click', function() { moveCarousel(-1); });
+        nextButton.addEventListener('click', function() { moveCarousel(1); });
 
-  updateCarousel();
-});
-
+        updateCarousel();
+    });
